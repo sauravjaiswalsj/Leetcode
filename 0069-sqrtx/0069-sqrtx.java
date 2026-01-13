90e552a28d0dfcde08ec388a4487204e4705f978
Saurav Jaiswal <sauravjaiswal999@gmail.com>
2026-01-13 17:23:22 +0530

    Time: 1 ms (96.3%), Space: 42.7 MB (46.81%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+        long low = 0, high = x;
+
+        while (low <= high){
+            long mid = low + (high - low)/2;
+
+            if (mid * mid == x)
+                return (int)mid;
+            else if (mid * mid > x)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return (int)low-1;
+    }
+}
