7d7e8227366ff25045cf8b607ecf83912b4ccd13
Saurav Jaiswal <sauravjaiswal999@gmail.com>
2026-02-19 01:22:11 +0000

    Time: 0 ms (100%), Space: 42.1 MB (57.18%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if (num == 1)
+            return true;
+        long l = 1, r = num;
+        while (l <= r){
+            long mid = l + (r - l)/2;
+            long sq = mid * mid;
+            if (sq == num)
+                return true;
+            else if (sq > num)
+                r = mid -1; 
+            else l = mid + 1;
+        }
+        return false;
+    }
+}
