f10b7760bf499d1039e2b14790b8b0b5dc3c1771
Saurav Jaiswal <sauravjaiswal999@gmail.com>
2026-02-21 03:38:53 +0000

    Time: 33 ms (89.37%), Space: 99.1 MB (41.54%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,36 +1,36 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        int i = 0, j = 0;
-        long max = 0;
+        int n = nums.length;
+        int left = 0;
         long sum = 0;
-        Set<Integer> set = new HashSet<>();
-        while (j < nums.length){
-            if (!set.contains(nums[j])){
-                set.add(nums[j]);
-                sum += nums[j];
-            }else{
-                set.remove(nums[i]);
-                sum -= nums[i];
-                i++;
-                continue;
+        long max = 0;
+        Set<Integer> seen = new HashSet<>();
+
+        for (int right = 0; right < n; right++) {
+            // 1) Ensure all elements are distinct by shrinking from left
+            while (seen.contains(nums[right])) {
+                seen.remove(nums[left]);
+                sum -= nums[left];
+                left++;
             }
 
-            if (j - i + 1 < k){
-                ++j;
-                continue;
+            // 2) Add current element
+            seen.add(nums[right]);
+            sum += nums[right];
+
+            // 3) If window too big (> k), shrink from left
+            if (right - left + 1 > k) {
+                seen.remove(nums[left]);
+                sum -= nums[left];
+                left++;
             }
-                
-            
-            if (j - i + 1 == k){
+
+            // 4) If window size == k, update answer
+            if (right - left + 1 == k) {
                 max = Math.max(max, sum);
-                ++j;
-                
-                sum -= nums[i];
-                set.remove(nums[i]);
-                ++i;
-                
             }
         }
+
         return max;
     }
 }
