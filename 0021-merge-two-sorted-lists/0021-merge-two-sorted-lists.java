cd00b4aa6bf5e838ec2958f032b2120fcec75308
Saurav Jaiswal <sauravjaiswal999@gmail.com>
2026-04-12 03:09:41 +0100

    Time: 0 ms (100%), Space: 44.1 MB (79.43%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,38 +10,27 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode l1_head = list1;
+        ListNode l2_head = list2;
         ListNode l3 = new ListNode();
-
-        return merge(list1, list2, l3);
-    }
-
-    private ListNode merge(ListNode l1, ListNode l2, ListNode res){
-        if (l1 == null && l2 == null)
-            return null;
-        
-        if (l1 == null){
-            res.next = l2;
-            return res.next;
+        ListNode head = l3;
+        while (l1_head != null && l2_head != null){
+            if (l1_head.val <= l2_head.val){
+                l3.next = l1_head;
+                l1_head = l1_head.next;
+            }
+            else{
+                l3.next = l2_head;
+                l2_head = l2_head.next;
+            }
+            l3 = l3.next;
         }
-            
-        if (l2 == null){
-            res.next = l1;
-            return res.next;
+        if (l1_head != null){
+            l3.next = l1_head;
         }
-
-        //System.out.println(res.val + " (res): "+ l1.val + " : "+ l2.val + " : com: " + (l1.val <= l2.val));
-        if (l1.val <= l2.val){
-            ListNode node = new ListNode(l1.val);
-            res.next = node;
-
-            merge(l1.next, l2, res.next);
+        if (l2_head != null){
+            l3.next = l2_head;
         }
-        else {
-            ListNode node = new ListNode(l2.val);
-            res.next = node;
-            merge(l1, l2.next, res.next);
-        }
-
-        return res.next;
+        return head.next;
     }
 }
