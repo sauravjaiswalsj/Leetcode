3602d691203c3280f7a8862df7939ce7a8bc2f31
Saurav Jaiswal <sauravjaiswal999@gmail.com>
2026-07-10 15:00:47 +0100

    Time: 2 ms (1.89%), Space: 44 MB (11.16%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,44 +1,47 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int pivot = getPivot(nums, nums.length);
+        if (nums == null || nums.length == 0)
+            return -1;
+        int n = nums.length;
+        int pivot = getPivot(nums, 0, n-1, n);
+        
         if (nums[pivot] == target)
             return pivot;
-
-        int firstHalf = binSearch(nums, 0, pivot, target);
-        int secondHalf = binSearch(nums, pivot, nums.length - 1, target);
         
-        return (firstHalf != -1 ? firstHalf : secondHalf != -1 ? secondHalf : -1); 
+        System.out.println(pivot);
+        int leftHalf = binSearch(nums, 0, pivot, target);
+        int rightHalf = binSearch(nums, pivot, n-1, target);
+        
+        return (leftHalf != -1 ? leftHalf : rightHalf != -1 ? rightHalf : -1);
     }
-
-    private int getPivot(int[] arr, int n){
-        int low = 0, high = n - 1;
-        if (arr[low] <= arr[high])
+    
+    private int getPivot(int[] nums, int low, int high, int n){
+        if (low > high)
+            return -1;
+        
+        if (nums[low] <= nums[high])
             return low;
-
-        while (low <= high){
-            int mid = low + (high - low)/2;
-
-            if (arr[mid] < arr[(mid + n -1)%n] && arr[mid] < arr[(mid + 1)%n])
+        
+        int mid = low + (high - low)/2;
+        
+        if (nums[mid] < nums[(mid + 1)%n] && nums[mid] < nums[(mid + n - 1)%n])
                 return mid;
-            if (arr[mid] < arr[high])
-                high = mid - 1;
-            else 
-                low = mid + 1;
-        }
-        return -1;
+        else if (nums[mid] < nums[high])
+            return getPivot(nums, low, mid - 1, n);
+        else
+            return getPivot(nums, mid +1, high, n);
     }
-
-    private int binSearch(int[] arr, int low, int high , int key){
-        while (low <= high){
-            int mid = low + (high - low)/2;
-
-            if (arr[mid] == key)
-                return mid;
-            else if (arr[mid] > key)
-                high = mid -1 ;
-            else 
-                low = mid + 1; 
-        }
-        return -1;
+    
+    private int binSearch(int[] nums, int low, int high, int target){
+        if (low > high)
+            return -1;
+        int mid = low + (high - low)/2;
+        
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[mid] < target)
+            return binSearch(nums, mid + 1, high, target);
+        else
+            return binSearch(nums, low, mid - 1, target);
     }
 }
